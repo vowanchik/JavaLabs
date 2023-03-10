89c2a41b016dfa77bbf13ebd7c142b818b9e0c4f
Vladimir <vowa.rozhnov@yandex.ru>
2023-03-10 23:35:50 +0300

    ЛР2, Вариант 1, Задача 6

---
+++ lab2.2/src/Main.java
@@ -0,0 +1,31 @@
+//Вариант 1, задача 6
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input a number of words: ");
+        int n = in.nextInt();
+        in.nextLine();
+        boolean flag = true;
+        String[] stringArray = new String[n];
+        for(int i = 0; i < n; i++) {
+            System.out.print("Input a word: ");
+            stringArray[i] = in.nextLine();
+        }
+        for (int i = 0; i < n; i++){
+            if (stringArray[i] != null && !stringArray[i].trim().isEmpty()){
+                for (int j = 0; j < stringArray[i].length() - 1; j++) {
+                    char symb = stringArray[i].charAt(j);
+                    char symb2 = stringArray[i].charAt(j + 1);
+                    if (symb >= symb2)
+                        flag = false;
+                }
+                if (flag) {
+                    System.out.println("This word is " + stringArray[i]);
+                    break;
+                }
+            }
+            flag = true;
+        }
+    }
+}
