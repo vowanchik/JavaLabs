bcf184d5350edb75847329d0d59f47fbba08b072
Vladimir <vowa.rozhnov@yandex.ru>
2023-03-10 21:33:40 +0300

    added comments

--- lab1.2/src/Main.java
+++ lab1.2/src/Main.java
@@ -1,11 +1,12 @@
+//Вариант 1, задача 4
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        String password = "ItIsPassword2023";
+        String password = "ItIsPassword2023"; //задаю пароль
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter password: ");
+        System.out.print("Enter password: "); //ввод пароля с консоли
         String enteredPassword = in.nextLine();
-        if (password.equals(enteredPassword))
+        if (password.equals(enteredPassword)) //проверка на совпадение паролей
             System.out.print("Password is correct!");
         else
             System.out.print("Password is not correct!");
